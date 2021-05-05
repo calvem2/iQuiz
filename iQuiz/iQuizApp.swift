4636af57c1395590638357a46985c2eaf46c711a
Megan <calvem2@cs.washington.edu>
2021-05-05 16:10:36 -0700

    Initial Commit

---
+++ iQuiz/iQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  iQuizApp.swift
+//  iQuiz
+//
+//  Created by Megan on 5/5/21.
+//
+
+import SwiftUI
+
+@main
+struct iQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
